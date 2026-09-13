b8b7cd345a31e7df6b293817e87e38f90be1451d
sarilevent <sarileve@gmail.com>
2026-09-13 14:00:47 -0500

    Update education details for faculty member

--- src/data/site.ts
+++ src/data/site.ts
@@ -11,7 +11,7 @@ export const site = {
   location: "Houston, Texas",
   office: "Science & Engineering Research Center (SERC), 4024",
   email: "lsari@uh.edu",
-  education: ["Ph.D., University of Georgia", "B.S., Bogazici University"],
+  education: ["Post-Doc., University of Michigan and UT-Southwestern", "Ph.D., University of Georgia", "B.S., Bogazici University"],
   facultyProfile: "https://www.uh.edu/nsm/biology-biochemistry/people/profiles/levent-sari/",
   googleScholar: "https://scholar.google.com/citations?hl=en&user=l2BhCiMAAAAJ",
   profilePhoto: "https://www.uh.edu/nsm/biology-biochemistry/people/profiles/levent-sari/profile-photo.jpg",
