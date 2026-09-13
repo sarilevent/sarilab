2ec5524aa4c5eb67a7b990c8759c50d23fd72dcf
sarilevent <sarileve@gmail.com>
2026-09-13 13:50:03 -0500

    Revise bio details for clarity and accuracy
    Updated research focus and interests in the bio section.

--- src/data/people.ts
+++ src/data/people.ts
@@ -14,11 +14,12 @@ export const people = [
   "where I worked with Henry F. Schaefer, and subsequently completed my postdoctoral training " +
   "at the University of Michigan with Ioan Andricioaei. Before joining the University of Houston, " +
   "I spent approximately nine years as a Senior Research Scientist in the Department of Bioinformatics " +
-  "at UT Southwestern Medical Center. At UTSW, I worked primarily in Milo Lin’s lab while developing " +
-  "close research collaborations with Lukasz Joachimiak and Marc Diamond at the Center for Alzheimer’s " +
-  "and Neurodegenerative Diseases, as well as with Jose Rizo-Rey in the Department of Biophysics. " +
-  "I continue to collaborate closely with several of these colleagues on ongoing research projects.",
-   interests: "I like tea, drinking tea, and having tea. Other than this, watching Messi and camping with my family and friends seem to make sense.. :)) ",
+  "at UT Southwestern Medical Center. At UTSW, my research focused primarily on the physical and biological " +
+   "aspects of protein aggregation and amyloid formation, which are major hallmarks of neurodegenerative diseases. " +
+   "While I worked primarily in Milo Lin’s lab, I also developed close research collaborations with Lukasz Joachimiak " + 
+   "and Marc Diamond at the Center for Alzheimer’s and Neurodegenerative Diseases, as well as with Jose Rizo-Rey " +
+   "in the Department of Biophysics. I continue to collaborate closely with several of these colleagues on ongoing research projects.",
+   interests: "I like tea, drinking tea, and having tea! Other than this, watching Messi and camping with my family and friends seem to make sense..:)) ",
     links: [
       { label: "UH Faculty Profile", href: site.facultyProfile },
       { label: "Google Scholar", href: site.googleScholar },
